6a791a8140a753269cdbfeef7946b33d9484aced
Katemar007 <ekaterinamarantidi@gmail.com>
2025-07-17 00:01:24 -0700

    Update BackendApiApplication.java

--- src/main/java/com/vocadabulary/backendapi/BackendApiApplication.java
+++ src/main/java/com/vocadabulary/backendapi/BackendApiApplication.java
@@ -1,1 +1,11 @@
+package com.vocadabulary.backendapi;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApiApplication.class, args);
+    }
+}
