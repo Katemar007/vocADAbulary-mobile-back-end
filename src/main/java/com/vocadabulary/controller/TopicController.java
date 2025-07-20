9a405ac67eef616d93e983ca8b53e29f52f55827
sashavershkova <a.m.vershkova@gmail.com>
2025-07-19 21:26:31 -0700

    create controller folder, TopicController.java

---
+++ src/main/java/com/vocadabulary/controller/TopicController.java
@@ -0,0 +1,21 @@
+package com.vocadabulary.controller;
+
+import com.vocadabulary.model.Topic;
+import com.vocadabulary.repository.TopicRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/topics")
+public class TopicController {
+
+    @Autowired
+    private TopicRepository topicRepository;
+
+    @GetMapping
+    public List<Topic> getAllTopics() {
+        return topicRepository.findAll();
+    }
+}
