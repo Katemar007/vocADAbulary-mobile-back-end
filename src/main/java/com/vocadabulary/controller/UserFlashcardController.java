1afec9b0b19d644111454e7466131fb218e8018b
sashavershkova <a.m.vershkova@gmail.com>
2025-07-20 21:37:37 -0700

    add get favotites of the uses

--- src/main/java/com/vocadabulary/controller/UserFlashcardController.java
+++ src/main/java/com/vocadabulary/controller/UserFlashcardController.java
@@ -1,28 +1,21 @@
 package com.vocadabulary.controller;
 
 import com.vocadabulary.model.UserFlashcard;
+import com.vocadabulary.model.FlashcardStatus;
 import com.vocadabulary.repository.UserFlashcardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import java.util.List;
-
 import java.util.List;
 
 @RestController
 @RequestMapping("/users/{userId}/flashcards")
 public class UserFlashcardController {
 
-    private final UserFlashcardRepository userFlashcardRepository;
+    @Autowired
+    private UserFlashcardRepository userFlashcardRepository;
 
-    public UserFlashcardController(UserFlashcardRepository userFlashcardRepository) {
-        this.userFlashcardRepository = userFlashcardRepository;
-    }
-
-    @GetMapping
-    public List<UserFlashcard> getAllFlashcardsForUser(@PathVariable Long userId) {
-        return userFlashcardRepository.findByUserId(userId);
+    @GetMapping("/favorites")
+    public List<UserFlashcard> getFavoriteFlashcards(@PathVariable Long userId) {
+        return userFlashcardRepository.findByUserIdAndStatus(userId, FlashcardStatus.FAVORITE);
     }
 }
--- src/main/java/com/vocadabulary/repository/UserFlashcardRepository.java
+++ src/main/java/com/vocadabulary/repository/UserFlashcardRepository.java
@@ -1,11 +1,9 @@
-package com.vocadabulary.repository;
-
 import com.vocadabulary.model.UserFlashcard;
 import com.vocadabulary.model.UserFlashcardKey;
+import com.vocadabulary.model.FlashcardStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface UserFlashcardRepository extends JpaRepository<UserFlashcard, UserFlashcardKey> {
-    List<UserFlashcard> findByUserId(Long userId);
+    List<UserFlashcard> findByUserIdAndStatus(Long userId, FlashcardStatus status);
 }
