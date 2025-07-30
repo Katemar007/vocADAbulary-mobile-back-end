2472d7703adb3b0d56a910e201315330b0c7eba7
sashavershkova <a.m.vershkova@gmail.com>
2025-07-30 16:09:40 -0700

    delete old quizzes schema files

--- src/main/java/com/vocadabulary/repositories/QuizAnswerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.vocadabulary.repositories;
-
-import com.vocadabulary.models.QuizAnswer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface QuizAnswerRepository extends JpaRepository<QuizAnswer, Long> {
-}
