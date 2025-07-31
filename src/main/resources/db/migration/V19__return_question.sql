691ef7f68ab99366dc9f411da55bbfa70e10742a
sashavershkova <a.m.vershkova@gmail.com>
2025-07-30 20:06:13 -0700

    delete old migrations

--- src/main/resources/db/migration/V19__return_question.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS quiz_questions (
-    id SERIAL PRIMARY KEY,
-    quiz_id INT NOT NULL REFERENCES quizzes(id),
-    question_text TEXT NOT NULL
-    -- no question_order column!
-);
