316e90aedc8f7e0a1c73578a6daee49a27a686a6
KateM <ekaterinamarantidi@gmail.com>
2025-08-08 00:25:30 -0700

    Remove ConstructorController

--- src/main/java/com/vocadabulary/VocadabularyApplication.java
+++ src/main/java/com/vocadabulary/VocadabularyApplication.java
@@ -1,10 +1,22 @@
 package com.vocadabulary;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 @SpringBootApplication
 public class VocadabularyApplication {
+
+//     @Bean
+// public CommandLineRunner showMappings(RequestMappingHandlerMapping handlerMapping) {
+//     return args -> {
+//         handlerMapping.getHandlerMethods().forEach((mapping, method) -> {
+//             System.out.println(mapping + " => " + method);
+//         });
+//     };
+//     }
     public static void main(String[] args) {
         SpringApplication.run(VocadabularyApplication.class, args);
     }
