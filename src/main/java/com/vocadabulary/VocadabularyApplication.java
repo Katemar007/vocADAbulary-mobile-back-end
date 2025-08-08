3b34cc341f664e4af0eb335e3f1f178b5864649a
KateM <ekaterinamarantidi@gmail.com>
2025-08-07 23:09:01 -0700

    untracked endpoints data feed

--- src/main/java/com/vocadabulary/VocadabularyApplication.java
+++ src/main/java/com/vocadabulary/VocadabularyApplication.java
@@ -9,14 +9,14 @@ import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandl
 @SpringBootApplication
 public class VocadabularyApplication {
 
-    @Bean
-public CommandLineRunner showMappings(RequestMappingHandlerMapping handlerMapping) {
-    return args -> {
-        handlerMapping.getHandlerMethods().forEach((mapping, method) -> {
-            System.out.println(mapping + " => " + method);
-        });
-    };
-    }
+//     @Bean
+// public CommandLineRunner showMappings(RequestMappingHandlerMapping handlerMapping) {
+//     return args -> {
+//         handlerMapping.getHandlerMethods().forEach((mapping, method) -> {
+//             System.out.println(mapping + " => " + method);
+//         });
+//     };
+//     }
     public static void main(String[] args) {
         SpringApplication.run(VocadabularyApplication.class, args);
     }
